87b4519684f80b941cce73a67906f2ab4fb26744
ethan-shenker <eshenker10@stuy.edu>
2020-10-01 21:23:53 -0400

    added K07

---
+++ 07_py-csv/K07.py
@@ -0,0 +1,29 @@
+# Team "Ethan and Dean"
+# Ethan Shenker, Ethan Machleder, Dean Carey
+# K06 -- Random(-ish) Team Occupation Selector
+# 2020-09-30
+# requires python3.8
+
+import csv
+from random import choices as c
+
+with open("occupations.csv") as file:  # open the file
+    reader = csv.DictReader(file)
+
+    jobs = []
+    weights = []
+
+    # create each list
+
+    for row in reader:
+        jobs.append(row["Job Class"])
+        weights.append(row['Percentage'])
+
+file.close()
+
+jobs = jobs[:-1]  # takes out last "total" value
+
+# converts all string values into floats, removes last
+weights = [float(val) for val in weights[:-1]]
+
+print(c(jobs, weights=weights))
